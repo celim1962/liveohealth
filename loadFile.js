423b4cadd9f4343066ae1246cda8feb1ebf70623
hungyeelin <hungyeelin@gmail.com>
2025-11-01 17:37:54 +0800

    update

--- loadFile.js
+++ loadFile.js
@@ -1,13 +1,9 @@
-async function includeHTML(id, file) {
-    const res = await fetch(file);
-    const html = await res.text();
-    document.getElementById(id).innerHTML = html;
-}
-
 document.addEventListener("DOMContentLoaded", async() => {
 
     let componentPath =window.location.pathname.endsWith("index.html")? './':'../';
-    let linkPath = window.location.pathname.endsWith("index.html")? './Content/':'./';
+    let linkPath = (
+      window.location.pathname.endsWith("index.html")||
+    window.location.pathname=='/')? './Content/':'./';
 
 
     await includeHTML("marquee", componentPath+"marquee.html");
@@ -17,17 +13,40 @@ document.addEventListener("DOMContentLoaded", async() => {
     document.getElementById('home').href = componentPath + 'index.html';
 
     await document.querySelectorAll('.menu-item').forEach(element => {
-      element.querySelector('a').href = linkPath + element.querySelector('a').getAttribute('href').split('/').pop();
+        if(element.querySelector('a')!=null){
+        element.querySelector('a').href = linkPath + element.querySelector('a').getAttribute('href').split('/').pop();
+      }
     });
 
-    
 
+    let allLink = document.getElementById('linkToProd')
+    Array.from(allLink.children).forEach(link=>{
+      link.addEventListener('click',el=>{
+        window.location.href= linkPath+ link.getAttribute('data-name')+'.html'
+      })
+    }) 
 
     await setupFooterImage(); 
 
 
+        const toggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+    const icon = toggle.querySelector("i");
+
+    toggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+        icon.classList.toggle("fa-bars");
+        icon.classList.toggle("fa-xmark");
+    });
+
 })
 
+
+async function includeHTML(id, file) {
+    const res = await fetch(file);
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
+}
 
 function setupFooterImage() {
   const img = document.getElementById("qrCodeImg");
